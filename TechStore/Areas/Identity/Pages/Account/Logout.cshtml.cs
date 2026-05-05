251ecd655d1e640be65cd5e770e2a67e61613075
mohamed68909 <m6377116@gmail.com>
2026-05-06 00:06:09 +0300

    Fix Logout 500 error by using ApplicationUser in SignInManager

--- TechStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ TechStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
@@ -14,10 +14,10 @@ namespace TechStore.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<TechStore.Entities.Models.ApplicationUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<TechStore.Entities.Models.ApplicationUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
